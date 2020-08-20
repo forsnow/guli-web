8f0f189c4296c7d749c6e0c68d54aa4a12203b33
forsnow <icxuesong@163.com>
2020-08-20 18:24:48 +0800

    test

--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -20,15 +20,26 @@
   <el-form-item label="课程分类">
     <el-select
       v-model="courseInfo.subjectParentId"
-      placeholder="一级分类">
+      placeholder="一级分类" @change="getSecondSubject">
       <el-option
-        v-for="subject1 in subjectFisrtList"
-        :key="subject1.id"
-        :label="subject1.title"
-        :value="subject1.id"/>
+        v-for="subject in subjectFisrtList"
+        :key="subject.id"
+        :label="subject.title"
+        :value="subject.id"/>
+    </el-select>
+
+     <el-select
+      v-model="courseInfo.subjectId"
+      placeholder="二级分类">
+      <el-option
+        v-for="subject in subjectSecondList"
+        :key="subject.id"
+        :label="subject.title"
+        :value="subject.id"/>
     </el-select>
   </el-form-item>
 
+  
   <!-- 课程讲师 TODO -->
   <el-form-item label="课程讲师">
     <el-select
@@ -48,10 +59,22 @@
 
   <!-- 课程简介 TODO -->
   <el-form-item label="课程简介">
-    <el-input v-model="courseInfo.description" placeholder="请填写课程的简介"/>
+     <el-input v-model="courseInfo.description" placeholder="请输入课程描述"/>
   </el-form-item>
 
   <!-- 课程封面 TODO -->
+  <el-form-item label="课程封面">
+
+  <el-upload
+    :show-file-list="false"
+    :on-success="handleAvatarSuccess"
+    :before-upload="beforeAvatarUpload"
+    :action="BASE_API+'/eduoss/file'"
+    class="avatar-uploader">
+    <img :src="courseInfo.cover">
+  </el-upload>
+
+</el-form-item>
 
   <el-form-item label="课程价格">
     <el-input-number :min="0" v-model="courseInfo.price" controls-position="right" placeholder="免费课程请设置为0元"/> 元
@@ -67,8 +90,10 @@
 <script>
 import course from "@/api/edu/course/course";
 import subject from '@/api/edu/subject/subject' 
+import Tinymce from '@/components/Tinymce'
 
 export default {
+    components: { Tinymce },
     data(){
         return{
             saveBtnDisabled: false,
@@ -80,11 +105,13 @@ export default {
                  lessonNum: 0,
                  description: '',
                  cover: '',
-                 price: 0
+                 price: 0,
+                 cover: '/static/01.png'
             },
             teacherList: [],
             subjectFisrtList: [],
-            subjectSecondList: []
+            subjectSecondList: [],
+            BASE_API: process.env.BASE_API//接口API地址
         }
     },
 
@@ -119,7 +146,46 @@ export default {
             subject.getNestedTreeList().then(response =>{
                 this.subjectFisrtList = response.data.list;
             })
+        },
+
+        //点击某一个一级分类，触发change 显示对应的二级分类
+        getSecondSubject(value){
+            console.log(value)
+            //console.log(this.subjectFisrtList)
+            for (var i = 0; i < this.subjectFisrtList.length; i++) {
+               if(this.subjectFisrtList[i].id == value){
+                   this.subjectSecondList = this.subjectFisrtList[i].children;
+                   this.courseInfo.subjectId = ''
+               }
+            }
+        },
+        //上传封面成功调用的方法
+        handleAvatarSuccess(){
+             console.log(res)// 上传响应
+             console.log(URL.createObjectURL(file.raw))// base64编码
+             this.courseInfo.cover = res.data.url
+        },
+
+        //上传之前
+        beforeAvatarUpload(){
+             const isJPG = file.type === 'image/jpeg'
+             const isLt2M = file.size / 1024 / 1024 < 2
+
+             if (!isJPG) {
+               this.$message.error('上传头像图片只能是 JPG 格式!')
+             }
+             if (!isLt2M) {
+               this.$message.error('上传头像图片大小不能超过 2MB!')
+             }
+             return isJPG && isLt2M
         }
+
     }
 }
 </script>
+
+<style scoped>
+.tinymce-container {
+  line-height: 29px;
+}
+</style>
